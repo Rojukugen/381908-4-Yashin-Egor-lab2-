a8c72a1549d4d1c4b419f71036e2619d2af00285
unknown <wizililqq@gmail.com>
2020-10-11 19:29:12 +0300

    Первая реализация

--- Matrix/main.cpp
+++ Matrix/main.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
 #include "MyVector.h"
-
+#include "Matrix.h"
 int main()
 {
   Vector<int> a(3, 3);
-
+  TMatrix<int> A(3);
+  TMatrix<int> B = A;
+  TMatrix<int> C(3);
+  C = A + B;
   return 0;
 }
